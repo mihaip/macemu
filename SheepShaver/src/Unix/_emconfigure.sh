71216e16b7c2002caa73502327b1d516f80bf103
Mihai Parparita <mihai@persistent.info>
2023-05-28 18:19:28 +0200

    Switch to module output for emscripten builds
    Results in smaller outputs, and unblocks the switch to Vite.
    
    Updates mihaip/infinite-mac#155

--- SheepShaver/src/Unix/_emconfigure.sh
+++ SheepShaver/src/Unix/_emconfigure.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 em_flags=""
 em_ldflags=""
 
-em_ldflags+=" -s INITIAL_MEMORY=268435456"
-em_ldflags+=" -s FORCE_FILESYSTEM=1"
-em_ldflags+=" -s WASM=1"
-
 em_defines=""
 if [[ -z "${macemujs_conf_debug:-}" ]]; then
   em_flags+=" -O3 -gsource-map"
@@ -19,16 +15,13 @@ else
   em_defines+=" -DDEBUG"
 fi
 
-export CC="emcc"
-export CXX="em++"
-export AR="emar"
 export EMSCRIPTEN=1
 export DEFINES=$em_defines
 export CFLAGS="$em_flags -g"
 export CPPFLAGS="$em_flags -g"
 export LDFLAGS="$em_flags $em_ldflags"
 
-./autogen.sh \
+emconfigure ./autogen.sh \
   --without-esd \
   --without-gtk \
   --disable-sdl-video \
